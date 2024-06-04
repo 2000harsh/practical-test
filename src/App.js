6d323cb3d145110dabaa6d89fdb6482dd631405f
harsh7harsh <harshbhatti1989@gmail.com>
2024-06-04 12:44:48 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
 import TicketDetails from "./TicketBooking/TicketDetails";
 import ViewTicket from "./TicketBooking/ViewTicket";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  useNavigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      
-
       <Routes>
-        <Route
-          path="/"
-          element={
-            <Userform
-              addDate={addDate}
-              editData={editData}
-              initialValues={
-                editingIndex !== null ? submittedData[editingIndex] : null
-              }
-            />
-          }
-        />
+        <Route path="/" element={<ViewTicket />} />
+        <Route path="/ticketDetails" element={<TicketDetails />} />
+      </Routes>
     </div>
   );
 }
