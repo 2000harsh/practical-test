c3fbaafcc654c37fbc0391123716f71e2ca19a20
harsh7harsh <harshbhatti1989@gmail.com>
2024-06-04 12:57:37 +0530

    ...

--- src/TicketBooking/TicketDetails.js
+++ src/TicketBooking/TicketDetails.js
@@ -2,15 +2,15 @@ import { Form, Input } from "antd";
 import React, { useState } from "react";
 
 const TicketDetails = () => {
-  const handleSubmit = (values) => {
+  const onFinish = (values) => {
     console.log(values);
   };
 
   return (
     <div>
       <h2>Ticket Details</h2>
-      <Form form={form} onSubmit={handleSubmit}>
-        <Form.Item>
+      <Form form={form} onFinish={onFinish} initialValues={{ fname: "" }}>
+        <Form.Item label="name" value>
           <Input />
         </Form.Item>
       </Form>
