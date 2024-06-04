fdf3e61e53119e5ee0d65fc7f35551f5fc667556
harsh7harsh <harshbhatti1989@gmail.com>
2024-06-04 12:53:43 +0530

    ...

--- src/TicketBooking/TicketDetails.js
+++ src/TicketBooking/TicketDetails.js
@@ -1,9 +1,19 @@
-import React from "react";
+import { Form, Input } from "antd";
+import React, { useState } from "react";
 
 const TicketDetails = () => {
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
+
   return (
     <div>
       <h2>Ticket Details</h2>
+      <Form form={form} onSubmit={handleSubmit}>
+        <Form.Item>
+          <Input />
+        </Form.Item>
+      </Form>
     </div>
   );
 };
